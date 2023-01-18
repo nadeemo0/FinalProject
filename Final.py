31d222a0ad0a4a028bebdeec4baae09c897a6b17
Nadeemo <Nadeem.rawashdeh21@gmail.com>
2023-01-18 22:48:33 +0300

    added to checkwin function

--- Final.py
+++ Final.py
@@ -1,10 +1,11 @@
 # Print welcome message 
 def intro():
+    print("Hello, and welcome to tic tac toe")
 
 board = [[" " for _ in range(3)] for _ in range(3)]
 
 def dboard():
-    print("1 2 3")
+    print("0 1 2")
     for i in range(3):
         print(i, end=" ")
     for j in range(3):
@@ -12,10 +13,19 @@ def dboard():
         print()
 
 def move():
+    # asks user to input row and column
     row = int(input(f"{player}, enter a row:"))
     column = int(input(f"{player}, enter a column:"))
-    if row in range(3) and column in range(3) and board[row][column] = " ":
+    # moving row and column
+    if row in range(3) and column in range(3) and board[row][column] == " ":
+        return row, column
+    
 def checkwin():
+    # check column
+    if board[0][column] == player and board[1][column] == player and board[2][column] == player:
+        return
+    # check diagonals
+    
 
 def main():
 
