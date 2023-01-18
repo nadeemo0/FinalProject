667616209dc268c08a4073b3159417cab02d9fc4
Nadeemo <Nadeem.rawashdeh21@gmail.com>
2023-01-18 22:29:56 +0300

    made board and made draw board function

--- Final.py
+++ Final.py
@@ -1,12 +1,20 @@
 # Print welcome message 
 def intro():
 
-board = 
+board = [[" " for _ in range(3)] for _ in range(3)]
 
-def draw_board():
+def dboard():
+    print("1 2 3")
+    for i in range(3):
+        print(i, end=" ")
+    for j in range(3):
+        print(board[i][j], end=" ")
+        print()
 
 def move():
-
+    row = int(input(f"{player}, enter a row:"))
+    column = int(input(f"{player}, enter a column:"))
+    if row in range(3) and column in range(3) and board[row][column] = " ":
 def checkwin():
 
 def main():
