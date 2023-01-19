7e5e88e9f72a8c0d21a798b2e6fee1305adb7262
Nadeemo <Nadeem.rawashdeh21@gmail.com>
2023-01-19 16:35:16 +0300

    added main function that calls everything

--- Final.py
+++ Final.py
@@ -7,15 +7,15 @@ def intro():
 board = [[" " for _ in range(3)] for _ in range(3)]
 
 def dboard():
-    print("0 1 2")
+    print("  0 1 2")
     for i in range(3):
         print(i, end=" ")
     for j in range(3):
         print(board[i][j], end=" ")
-        print()
+    print()
 
-def move():
-    while:
+def move(player):
+    while True:
         try:
             # asks user to input row and column
             row = int(input(f"{player}, enter a row:"))
@@ -24,14 +24,14 @@ def move():
             if row in range(3) and column in range(3) and board[row][column] == " ":
                 return row, column
             print("Invalid move, try again")
-        except ValueError
+        except ValueError:
             print("Invalid move, try again")
     
-def checkwin():
+def checkwin(player):
     # check column
     for column in range(3):
         if board[0][column] == player and board[1][column] == player and board[2][column] == player:
-            return
+            return True
     # check diagonals
     if board[0][2]== player and board[1][1] == player and board[2][0] == player:
         return True
@@ -39,12 +39,28 @@ def checkwin():
         return True
     # check row
     for row in range(3):
-        if board[row] == [player, player, player]
+        if board[row] == [player, player, player]:
+            return True
     return False
 
 
 def main():
-
+    dboard()
+    while True:
+        # prompt for X's turn
+        row, column = move("X")
+        board[row][column]
+        dboard()
+        if checkwin("X"):
+            print("X wins")
+            break
+        # prompt for O's turn
+        row, column = move("O")
+        board[row][column]
+        dboard()
+        if checkwin("O"):
+            print("O wins")
+            break
 
 if __name__ == "__main__":
     main()
